3fc49f5fad1a20a8eb65fdaae18e18e62cad518c
Eliyar Eziz <eliyar917@gmail.com>
2021-11-24 21:59:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with codecs.open('requirements.txt', 'r', 'utf8') as reader:
 
 setuptools.setup(
     name="yodo1",
-    version="0.0.9",
+    version="0.1.0",
     author="Eliyar Eziz",
     author_email="eliyar@yodo1.com",
     description="A Yodo1 Python Toolbox",
