1e0fdae4f4cacd0649d5ccf45e9a4875f0b3e831
BrikerMan <eliyar917@gmail.com>
2021-11-27 02:37:02 +0800

    ✨ Add estimated time to progress bar.

--- yodo1/progress.py
+++ yodo1/progress.py
@@ -1,10 +1,12 @@
 import logging
+import time
+import random
 
 logger = logging.getLogger('yodo1.progress')
 
 
 class ProgressBar:
-    WIDTH = 50
+    WIDTH = 30
 
     def __init__(self, total: int, desc: str, step: int = 50) -> None:
         self.index = 0
@@ -12,7 +14,24 @@ class ProgressBar:
         self.step = step
         self.desc = desc
 
+        self._start_at = 0
+
+    def _format_time(self, secs: float) -> str:
+        if secs < 3600:
+            return f"{secs // 60:02.0f}:{secs % 60:02.0f}"
+        else:
+            return f"{secs // 3600:02.0f}:{secs // 60:02.0f}:{secs % 60:02d}"
+
+    def _get_time_string(self) -> str:
+        spend_time = time.time() - self._start_at
+        estimated_time = spend_time / self.index * self.total
+
+        return f"{self._format_time(spend_time)}<{self._format_time(estimated_time)}"
+
     def update(self, n: int = 1) -> None:
+        if self.index == 0:
+            self._start_at = time.time()
+
         self.index += n
 
         if self.index % self.step == 0 or self.index == self.total:
@@ -21,8 +40,8 @@ class ProgressBar:
                 fill_count = ProgressBar.WIDTH
 
             progress = f" {100 * self.index / self.total:.1f}%"
-            text = f"{progress:<8s}|" + '>' * fill_count + ' ' * (ProgressBar.WIDTH - fill_count) + "| " + f"{self.index}/{self.total} "
-            text += self.desc
+            text = f"{progress:<8s}|" + '#' * fill_count + '-' * (ProgressBar.WIDTH - fill_count) + "| " + f"{self.index}/{self.total} "
+            text += f"[{self._get_time_string()}] {self.desc}"
             logger.info(text)
 
 
@@ -30,7 +49,8 @@ if __name__ == '__main__':
     import logging
 
     logging.basicConfig(level='DEBUG')
-    p = ProgressBar(total=100, desc="Hacking ...", step=12)
+    p = ProgressBar(total=20, desc="Hacking ...", step=20//6)
 
-    for i in range(100):
+    for i in range(20):
         p.update()
+        time.sleep(random.randint(0, 60) / 300)
