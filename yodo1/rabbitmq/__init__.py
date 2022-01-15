2288ab4d67da7450322d055b29d5f5e5ecfa9e7e
Eliyar Eziz <eliyar917@gmail.com>
2022-01-16 00:16:34 +0800

    ♻️ Optimize import

--- yodo1/rabbitmq/__init__.py
+++ yodo1/rabbitmq/__init__.py
@@ -1,2 +1,2 @@
-from .multi_thread import MultiThreadConsumer  # noqa: F401
+from .multi_thread import MultiThreadConsumer, CallbackResult, MQAction  # noqa: F401
 from .http_client import RabbitHttpSender  # noqa: F401
